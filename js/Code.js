9f62649cbcab5858a4d6a99b1aba02990a53bdfb
252009 <84382723+252009@users.noreply.github.com>
2021-08-28 20:03:53 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "ROBLOX";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    fill("cyan")
+    textSize(15)
+    text("R B O X L O", 700,150)
+    fill("lightblue")
+    text("Hint:VARIETY OF GAMING AVAILABLE ON PLAY STORE!!", 700,170)
+    textSize(15)
+    text("B A D A S E T A", 100,250)
+    fill("lightblue")
+    text("Hint: STORAGE FOR INFORMATION!!", 100,270)
+    
+
+
+    
+
+}
